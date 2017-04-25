56fa2a6464ca19c30f764e159fbdfd65d5a1b030
Dmitry Turovtsov <web.best.master@gmail.com>
2017-04-25 10:18:07 +0300

    initial commit

--- models/User.js
+++ models/User.js
@@ -22,6 +22,12 @@ User.schema.virtual('canAccessKeystone').get(function () {
 
 
 /**
+ * Relationships
+ */
+User.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
+
+
+/**
  * Registration
  */
 User.defaultColumns = 'name, email, isAdmin';
--- routes/middleware.js
+++ routes/middleware.js
@@ -1,12 +1,28 @@
+/**
+ * This file contains the common middleware used by your routes.
+ *
+ * Extend or replace these functions as your application requires.
+ *
+ * This structure is not enforced, and just a starting point. If
+ * you have more middleware you may want to group it as separate
+ * modules in your project's /lib directory.
+ */
 var _ = require('lodash');
 
 
 /**
 	Initialises the standard view locals
+
+	The included layout depends on the navLinks array to generate
+	the navigation in the header, you may wish to change this array
+	or replace it with your own templates / logic.
 */
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
+		{ label: 'Blog', key: 'blog', href: '/blog' },
+		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+		{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
 	next();
